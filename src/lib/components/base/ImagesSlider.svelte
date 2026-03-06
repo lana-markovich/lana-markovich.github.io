9a9ba1d9b3e2495daffa2bb535e767688d42c9f8
Kirill Sakun <kirillsakun03@gmail.com>
2026-03-06 09:35:25 +0100

    Fix: Fix image position and colors

--- src/lib/components/base/ImagesSlider.svelte
+++ src/lib/components/base/ImagesSlider.svelte
@@ -11,28 +11,38 @@
 		<BaseImage
 			name={image.id}
 			alt={image.alt}
-			class={image.id === currentImage ? 'is-active' : 'is-hidden'}
+			class={`images-slider__image ${image.id === currentImage ? 'images-slider__image--is-active' : 'images-slider__image--is-hidden'}`}
 			sizes={image.sizes}
 		/>
 	{/each}
 </div>
 
 <style>
-	.images-slider :global(.is-hidden) {
-		opacity: 0;
-		transition: opacity 1s ease;
-	}
-
-	.images-slider :global(.is-active) {
-		opacity: 1;
-		transition: opacity 1s ease;
-	}
-
-	.images-slider :global(.is-hidden),
-	.images-slider :global(.is-active) {
+	.images-slider {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		position: absolute;
-		top: 0;
-		left: 0;
+		inset: 0;
+		z-index: 0;
+		overflow: hidden;
+		:global(.images-slider__image) {
+			transition: opacity 0.7s ease;
+			opacity: 0;
+			position: absolute;
+			max-height: unset;
+			&::before {
+				content: '';
+				display: block;
+				position: absolute;
+				background-color: var(--black-800);
+				opacity: 0.6;
+				inset: 0;
+				z-index: 2;
+			}
+		}
+		:global(.images-slider__image--is-active) {
+			opacity: 1;
+		}
 	}
-
 </style>
