65ae1380491a828acba5bd683ddacb795b9bf8f5
Kirill Sakun <kirillsakun03@gmail.com>
2026-03-29 17:10:04 +0200

    feat: add ART_PIECES constant with all 18 art pieces

---
+++ src/lib/constants.ts
@@ -0,0 +1,109 @@
+export * from "./generated/constants";
+
+import type { ArtPiece } from './types.js';
+import type { ImageId } from './generated/types.js';
+
+export const ART_PIECES: Partial<Record<ImageId, ArtPiece>> = {
+	'melancholy': {
+		name: "Melancholy",
+		year: 2020,
+		description: "100*70 cm, charcoal on paper",
+		image: { id: "melancholy", alt: "Melancholy" }
+	},
+	'nostalgia': {
+		name: "nostalgia",
+		year: 2020,
+		description: "100*70 cm, mixed media",
+		image: { id: "nostalgia", alt: "nostalgia" }
+	},
+	'isolation': {
+		name: "isolation",
+		year: 2020,
+		description: "100*140 cm, mixed media",
+		image: { id: "isolation", alt: "isolation" }
+	},
+	'confusion': {
+		name: "confusion",
+		year: 2020,
+		description: "100*70 cm, charcoal on paper",
+		image: { id: "confusion", alt: "confusion" }
+	},
+	'what-does-it-mean': {
+		name: "what does it mean",
+		year: 2020,
+		description: "100*70 cm, mixed media",
+		image: { id: "what-does-it-mean", alt: "what does it mean" }
+	},
+	'there-is-no-more-reality': {
+		name: "there is no more reality",
+		year: 2020,
+		description: "100*70 cm, mixed media",
+		image: { id: "there-is-no-more-reality", alt: "there is no more reality" }
+	},
+	'time-is-fleeting': {
+		name: "time is fleeting",
+		year: 2019,
+		description: "70*50 cm, mixed media",
+		image: { id: "time-is-fleeting", alt: "time is fleeting" }
+	},
+	'reality-doesnt-exist': {
+		name: "reality doesn't exist",
+		year: 2020,
+		description: "100*70 cm, charcoal on paper",
+		image: { id: "reality-doesnt-exist", alt: "reality doesn't exist" }
+	},
+	'nothing': {
+		name: "nothing",
+		year: 2018,
+		description: "100*140 cm, mixed media",
+		image: { id: "nothing", alt: "nothing" }
+	},
+	'forbidden-fruit-is-the-sweetest': {
+		name: "forbidden fruit is the sweetest",
+		year: 2015,
+		description: "120*60 cm, mixed media",
+		image: { id: "forbidden-fruit-is-the-sweetest", alt: "forbidden fruit is the sweetest" }
+	},
+	'portrait-1': {
+		name: "portrait 1",
+		year: 2021,
+		description: "100*70 cm, mixed media",
+		image: { id: "portrait-1", alt: "portrait 1" }
+	},
+	'portrait-2': {
+		name: "portrait 2",
+		year: 2022,
+		description: "21*29,7 cm, pencil",
+		image: { id: "portrait-2", alt: "portrait 2" }
+	},
+	'anatomy-1': {
+		name: "anatomy 1",
+		year: 2022,
+		description: "21*29,7 cm, mixed media",
+		image: { id: "anatomy-1", alt: "anatomy 1" }
+	},
+	'anatomy-2': {
+		name: "anatomy 2",
+		year: 2023,
+		description: "21*29,7 cm, mixed media",
+		image: { id: "anatomy-2", alt: "anatomy 2" }
+	},
+	'deer': {
+		name: "deer",
+		year: 2022,
+		description: "140*200 cm, mixed media",
+		image: { id: "deer", alt: "deer" }
+	},
+	'abstract-1': {
+		name: "abstract-1",
+		image: { id: "abstract-1", alt: "abstract-1" }
+	},
+	'abstract-2': {
+		name: "abstract-2",
+		image: { id: "abstract-2", alt: "abstract-2" }
+	},
+	'abstract-3': {
+		name: "abstract-3",
+		image: { id: "abstract-3", alt: "abstract-3" }
+	},
+};
