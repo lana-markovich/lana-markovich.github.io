493d320b51b33be6baf5c2c240941f1721c56b8b
Kirill Sakun <kirillsakun03@gmail.com>
2026-03-29 19:57:56 +0200

    feat: add Portfolio section with jacket-1

---
+++ src/lib/components/sections/Portfolio.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import BaseSection from "$lib/components/layout/BaseSection.svelte";
+	import ArtPiecePreview from "$lib/components/base/ArtPiecePreview.svelte";
+	import { ART_PIECES } from "$lib/constants";
+
+	const piece = ART_PIECES['jacket-1'];
+</script>
+
+<BaseSection type="primary">
+	<ArtPiecePreview artPiece={piece} />
+</BaseSection>
