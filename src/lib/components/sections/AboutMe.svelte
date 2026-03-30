4400f1a0a4e35cf3d666716f16a1bcd0f2e5ad85
Kirill Sakun <kirillsakun03@gmail.com>
2026-03-30 22:30:38 +0200

    refactor: migrate AboutMe to FramedImage, remove hardcoded object-position CSS

--- src/lib/components/sections/AboutMe.svelte
+++ src/lib/components/sections/AboutMe.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import BaseSection from "$lib/components/layout/BaseSection.svelte";
 	import DecorationWrapper from "$lib/components/base/DecorationWrapper.svelte";
-	import BaseImage from "$lib/components/base/BaseImage.svelte";
+	import FramedImage from "$lib/components/base/FramedImage.svelte";
+	import { IMAGES } from "$lib/images";
 </script>
 
 <BaseSection type="secondary" class="about-me">
@@ -19,10 +20,7 @@
 
 		<div class="about-me__image-wrapper">
 			<DecorationWrapper decorationPosition="bottom-left">
-				<BaseImage
-					name="about-me"
-					alt="Sviatlana Markovich portrait"
-				/>
+				<FramedImage image={IMAGES['about-me']} />
 			</DecorationWrapper>
 		</div>
 	</div>
@@ -68,11 +66,6 @@
 		height: var(--fluid-height);
 		max-height: var(--max-height);
 		width: min(46.5rem, calc(var(--container-colunm-width) + var(--container-to-screen-side-width)));
-	}
-
-	:global(.image--about-me) {
-		object-fit: cover;
-		object-position: 50% 76%;
 	}
 
 </style>
