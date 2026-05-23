2c2ccf2ab7c0cfc816f0d009f3a3b5c72c98d8e4
Kirill Sakun <kirillsakun03@gmail.com>
2026-05-23 16:33:22 +0200

    Update: Split portfolio items into separate lists

--- src/lib/components/sections/Portfolio.svelte
+++ src/lib/components/sections/Portfolio.svelte
@@ -5,7 +5,9 @@
 	import type { ArtPieceId } from "$lib/constants";
 	import { sectionHeading } from "$lib/utilities/sectionHeading";
 
-	const ITEMS: { id: ArtPieceId; aspectRatio?: number; centerBadge?: boolean }[] = [
+	type Item = { id: ArtPieceId; aspectRatio?: number; centerBadge?: boolean };
+
+	const PAINTINGS: Item[] = [
 		{ id: 'reality-doesnt-exist' },
 		{ id: 'landscape-1', aspectRatio: 384 / 450 },
 		{ id: 'copy-la-grenouillere', centerBadge: true },
@@ -15,18 +17,49 @@
 		{ id: 'nothing' },
 		{ id: 'what-does-it-mean', aspectRatio: 384 / 450 },
 		{ id: 'isolation', centerBadge: true },
+	];
+
+	const DECOR: Item[] = [
 		{ id: 'jacket-1' },
 		{ id: 'dresser-decoration-storks', aspectRatio: 384 / 450 },
-		{ id: 'body-1', centerBadge: true },
+		{ id: 'shopper-bag-design-noise', centerBadge: true },
+	];
+
+	const GRAPHIC: Item[] = [
+		{ id: 'body-1' },
 		{ id: 'pikoala', aspectRatio: 408 / 450 },
-		{ id: 'shopper-bag-design-noise', aspectRatio: 384 / 600 },
 	];
 </script>
 
 <BaseSection type="primary" class="portfolio" id="portfolio">
 	<h2 class="heading heading--md" use:sectionHeading>Portfolio</h2>
-	<ul class="portfolio__items">
-		{#each ITEMS as item}
+
+	<ul class="portfolio__items portfolio__items--painting">
+		{#each PAINTINGS as item}
+			<li
+				class="portfolio__item"
+				class:portfolio__item--center-badge={item.centerBadge}
+				style="grid-area: {item.id}"
+			>
+				<ArtPiecePreview artPiece={ART_PIECES[item.id]} aspectRatio={item.aspectRatio}/>
+			</li>
+		{/each}
+	</ul>
+
+	<ul class="portfolio__items portfolio__items--decor">
+		{#each DECOR as item}
+			<li
+				class="portfolio__item"
+				class:portfolio__item--center-badge={item.centerBadge}
+				style="grid-area: {item.id}"
+			>
+				<ArtPiecePreview artPiece={ART_PIECES[item.id]} aspectRatio={item.aspectRatio}/>
+			</li>
+		{/each}
+	</ul>
+
+	<ul class="portfolio__items portfolio__items--graphic">
+		{#each GRAPHIC as item}
 			<li
 				class="portfolio__item"
 				class:portfolio__item--center-badge={item.centerBadge}
@@ -48,14 +81,28 @@
 		list-style: none;
 		grid-template-columns: var(--lines-columns-grid);
 		row-gap: 2.5rem;
-		grid-template-areas: "reality-doesnt-exist .                    landscape-1      "
-		                     ".                    copy-la-grenouillere .                "
-		                     "iris                 .                    imagination      "
-		                     ".                    still-life           .                "
-		                     "nothing              .                    what-does-it-mean"
-		                     ".                    isolation            .                "
-		                     "jacket-1             .                    dresser-decoration-storks"
-		                     ".                    body-1               .                "
-		                     "pikoala              .                    shopper-bag-design-noise";
+	}
+
+	.portfolio__items + .portfolio__items {
+		margin-top: 2.5rem;
+	}
+
+	.portfolio__items--painting {
+		grid-template-areas: "reality-doesnt-exist .                        landscape-1      "
+		                     ".                    copy-la-grenouillere     .                "
+		                     "iris                 .                        imagination      "
+		                     ".                    still-life               .                "
+		                     "nothing              .                        what-does-it-mean"
+		                     ".                    isolation                .                ";
+	}
+
+	.portfolio__items--decor {
+		grid-template-areas: "jacket-1             .                        dresser-decoration-storks"
+		                     ".                    shopper-bag-design-noise .                        ";
+	}
+
+	.portfolio__items--graphic {
+		width: 100%;
+		grid-template-areas: "body-1               .                        pikoala";
 	}
 </style>
