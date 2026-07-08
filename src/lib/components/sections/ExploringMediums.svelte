2c3c2078573f7e7c900eb1e787f6de7df3523b11
Kirill Sakun <kirillsakun03@gmail.com>
2026-07-09 00:06:27 +0200

    feature: add responsive exploring mediums section

---
+++ src/lib/components/sections/ExploringMediums.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import BaseSection from "$lib/components/layout/BaseSection.svelte";
+</script>
+
+<BaseSection class="exploring-mediums" id="exploring-mediums" tracked>
+	<div class="exploring-mediums__text">
+		<h2 class="heading heading--lg">
+			Exploring Mediums From Canvas to Everyday Objects
+		</h2>
+		<p class="text text--secondary">
+			I work with pigments, oil, and acrylic in abstract pieces, paint furniture and home decor, and create wearable art on clothing. My artwork also weaves social and ecological themes, revealing forces shaping modern life.
+		</p>
+	</div>
+</BaseSection>
+
+<style>
+	:global(.exploring-mediums .container) {
+		padding-block: 0;
+	}
+	.exploring-mediums__text {
+		display: grid;
+		grid-template-columns: var(--lines-columns-grid);
+		grid-template-areas: "heading heading heading"
+		                     ".       text    text";
+		row-gap: 1.5rem;
+		text-wrap-style: pretty;
+		@media (width <= 65rem) {
+			grid-template-areas: "heading heading"
+			                     ".       text";
+		}
+		@media (width <= 35rem) {
+			grid-template-areas: "heading"
+			                     "text";
+		}
+
+		.heading {
+			grid-area: heading;
+			max-width: 25ch;
+			margin-block-end: 0;
+		}
+		.text {
+			grid-area: text;
+			max-width: 52ch;
+			margin-block: 0;
+		}
+	}
+</style>
--- src/pages/HomePage.svelte
+++ src/pages/HomePage.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+import { MediaQuery } from "svelte/reactivity";
 import BasePage from "$lib/components/layout/BasePage.svelte";
 import StickyNav from "$lib/components/layout/StickyNav.svelte";
 import HomeHero from "$lib/components/sections/HomeHero.svelte";
@@ -9,6 +10,9 @@ import Footer from "$lib/components/sections/Footer.svelte";
 import CaptureEmotions from "$lib/components/sections/CaptureEmotions.svelte";
 import Services from "$lib/components/sections/Services.svelte";
 import Portfolio from "$lib/components/sections/Portfolio.svelte";
+import ExploringMediums from "$lib/components/sections/ExploringMediums.svelte";
+
+const isCompact = new MediaQuery("(max-width: 65rem)");
 </script>
 
 <BasePage>
@@ -16,9 +20,15 @@ import Portfolio from "$lib/components/sections/Portfolio.svelte";
 	<HomeHero/>
 	<HomeIntro/>
 	<AboutMe/>
+	{#if isCompact.current}
+		<ExploringMediums/>
+	{/if}
 	<ImageGallery/>
 	<CaptureEmotions/>
 	<Services/>
+	{#if !isCompact.current}
+		<ExploringMediums/>
+	{/if}
 	<Portfolio/>
 	<Footer/>
 </BasePage>
